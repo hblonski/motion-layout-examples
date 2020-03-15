8016d3fda32013e2ad78029d234ef93b574496fa
Hayllander <hayblonski@gmail.com>
2020-03-15 15:51:44 -0300

    Second example: drag circle

--- app/src/main/java/com/example/android/motionlayoutexamples/MainActivity.kt
+++ app/src/main/java/com/example/android/motionlayoutexamples/MainActivity.kt
@@ -2,18 +2,27 @@ package com.example.android.motionlayoutexamples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         click_to_move_button.setOnClickListener {
-            val fragmentTransaction = supportFragmentManager.beginTransaction()
-            val fragment = MoveOnClickFragment()
-            fragmentTransaction.addToBackStack(null)
-            fragmentTransaction.replace(R.id.fragment_container, fragment)
-            fragmentTransaction.commit()
+            replaceFragment(MoveOnClickFragment())
         }
+
+        drag_shape_button.setOnClickListener {
+            replaceFragment(DragCircleFragment())
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
